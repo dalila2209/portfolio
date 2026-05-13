a5f4110e53665d5b1960835818c12a42b91d3451
dalilaacqua <dacquavi@emeal.nttdata.com>
2026-05-13 12:48:37 +0200

    add cookie and privacy policies

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,25 @@ import './../App.css';
 const Footer: React.FC = () => {
     return (
         <footer className='footer-class rounded-t-lg'>
-            <p>© All rights reserved  ~  created using React</p>
+            <p>© All rights reserved ~ created using React</p>
+            <div className="footer-links">
+                <a
+                    href="https://www.iubenda.com/privacy-policy/44862765"
+                    className="iubenda-black iubenda-noiframe iubenda-embed"
+                    title="Privacy Policy"
+                >
+                    Privacy Policy
+                </a>
+                <a
+                    href="https://www.iubenda.com/privacy-policy/44862765/cookie-policy"
+                    className="iubenda-black iubenda-noiframe iubenda-embed"
+                    title="Cookie Policy"
+                >
+                    Cookie Policy
+                </a>
+            </div>
         </footer>
     )
 }
 
 export default Footer;
-
